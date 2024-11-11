45952c8a694957a14686f7d60f1391851e748364
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2024-11-12 04:37:28 +0530

    skill page done

--- src/app/skill/page.tsx
+++ src/app/skill/page.tsx
@@ -1,7 +1,66 @@
+import { Card, CardHeader } from "@/components/ui/card";
+import { BackendSkill, CloudData, SkillData } from "../../../utils/skillData";
+
 const page = () => {
   return (
     <>
-      <div className="">Skill</div>
+      <div className="grid gap-8 pb-16 pt-8">
+        {/* // frontend skill start  */}
+        <div className="bg-gradient-to-b from-neutral-500 to-neutral-200 bg-clip-text text-5xl font-bold text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 sm:text-6xl">
+          Fronted Skills
+        </div>
+        <div className="grid grid-cols-2 gap-5 sm:grid-cols-4">
+          {SkillData.map((item, index) => {
+            return (
+              <Card key={index}>
+                <CardHeader className="grid place-items-center gap-3">
+                  <item.icon size={item.iconSize} />
+                  <div className="bg-gradient-to-b from-neutral-500 to-neutral-200 bg-clip-text text-lg font-bold text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 sm:text-2xl">
+                    {item.name}
+                  </div>
+                </CardHeader>
+              </Card>
+            );
+          })}
+        </div>
+
+        {/* // backend skill start */}
+        <div className="bg-gradient-to-b from-neutral-500 to-neutral-200 bg-clip-text text-5xl font-bold text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 sm:text-6xl">
+          Backend Skills
+        </div>
+        <div className="grid grid-cols-2 gap-5 sm:grid-cols-4">
+          {BackendSkill.map((item, index) => {
+            return (
+              <Card key={index}>
+                <CardHeader className="grid place-items-center gap-3">
+                  <item.icon size={item.iconSize} />
+                  <div className="bg-gradient-to-b from-neutral-500 to-neutral-200 bg-clip-text text-lg font-bold text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 sm:text-2xl">
+                    {item.name}
+                  </div>
+                </CardHeader>
+              </Card>
+            );
+          })}
+        </div>
+        {/* // cloud computing skill start  */}
+        <div className="bg-gradient-to-b from-neutral-500 to-neutral-200 bg-clip-text text-5xl font-bold text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 sm:text-6xl">
+          Cloud Computing Skills
+        </div>
+        <div className="grid grid-cols-2 gap-5 sm:grid-cols-4">
+          {CloudData.map((item, index) => {
+            return (
+              <Card key={index}>
+                <CardHeader className="grid place-items-center gap-3">
+                  <item.icon size={item.iconSize} />
+                  <div className="bg-gradient-to-b from-neutral-500 to-neutral-200 bg-clip-text text-lg font-bold text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 sm:text-2xl">
+                    {item.name}
+                  </div>
+                </CardHeader>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
---
+++ utils/skillData.ts
@@ -0,0 +1,107 @@
+import { FaDigitalOcean, FaReact } from "react-icons/fa";
+import { FaAws, FaBootstrap, FaHtml5 } from "react-icons/fa6";
+import { IoLogoCss3, IoLogoJavascript } from "react-icons/io";
+import { RiNextjsLine, RiNodejsLine } from "react-icons/ri";
+import {
+  SiDirectus,
+  SiGooglecloud,
+  SiMicrosoftazure,
+  SiPostgresql,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
+import { TbBrandMysql, TbBrandVite } from "react-icons/tb";
+
+export const SkillData = [
+  {
+    icon: FaHtml5,
+    name: "Html5",
+    iconSize: 100,
+  },
+  {
+    icon: IoLogoCss3,
+    name: "Css3",
+    iconSize: 100,
+  },
+  {
+    icon: FaBootstrap,
+    name: "Bootstrap",
+    iconSize: 100,
+  },
+  {
+    icon: IoLogoJavascript,
+    name: "Javascript",
+    iconSize: 100,
+  },
+  {
+    icon: SiTypescript,
+    name: "Typescript",
+    iconSize: 100,
+  },
+  {
+    icon: SiTailwindcss,
+    name: "Tailwindcss",
+    iconSize: 100,
+  },
+  {
+    icon: FaReact,
+    name: "React",
+    iconSize: 100,
+  },
+  {
+    icon: TbBrandVite,
+    name: "Vite",
+    iconSize: 100,
+  },
+  {
+    icon: RiNextjsLine,
+    name: "Nextjs",
+    iconSize: 100,
+  },
+];
+
+export const BackendSkill = [
+  {
+    icon: RiNodejsLine,
+    name: "Nodejs",
+    iconSize: 100,
+  },
+  {
+    icon: SiDirectus,
+    name: "Directus",
+    iconSize: 100,
+  },
+  {
+    icon: SiPostgresql,
+    name: "Postgresql",
+    iconSize: 100,
+  },
+  {
+    icon: TbBrandMysql,
+    name: "Mysql",
+    iconSize: 100,
+  },
+];
+
+export const CloudData = [
+  {
+    icon: FaAws,
+    name: "Aws",
+    iconSize: 100,
+  },
+  {
+    icon: SiGooglecloud,
+    name: "Googlecloud",
+    iconSize: 100,
+  },
+  {
+    icon: SiMicrosoftazure,
+    name: "Microsoftazure",
+    iconSize: 100,
+  },
+  {
+    icon: FaDigitalOcean,
+    name: "DigitalOcean ",
+    iconSize: 100,
+  },
+];
