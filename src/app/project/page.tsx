e40316d145ec1cf3abe09f891178998075d52355
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2024-11-26 23:23:00 +0530

    project page done

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,7 +1,47 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { projectData } from "../../../utils/projectInfo";
+import { Button } from "@/components/ui/button";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+
 const page = () => {
   return (
     <>
-      <div className="">Project</div>
+      <div className="grid gap-8 pb-14 mt-20">
+        <div className="text-neutral-900 dark:bg-clip-text text-5xl font-bold dark:text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 sm:text-6xl">
+          All Project
+        </div>
+        <div className="grid grid-flow-row gap-8 sm:grid-cols-2">
+          {projectData.map((info, index) => (
+            <Card key={index}>
+              <CardHeader>
+                <CardTitle>
+                  <div className="text-3xl font-bold text-neutral-900 dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400">
+                    {info.title}
+                  </div>
+                </CardTitle>
+                <CardDescription>
+                  <p className="text-lg">
+                    <span className="font-bold">Description: </span>
+                    {info.desription}
+                  </p>
+                </CardDescription>
+              </CardHeader>
+
+              <CardContent>
+                <Button size={"lg"}>
+                  Github <FaArrowUpRightFromSquare />
+                </Button>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
---
+++ utils/projectInfo.ts
@@ -0,0 +1,22 @@
+export const projectData = [
+  {
+    title: "RandoUser",
+    desription:
+      "Meet random users, when users give one, two, or other number in input fields, explore unique profiles with just a Button click.",
+  },
+  {
+    title: "Currency Converter",
+    desription:
+      "Users Can Easily convert between different currencies with real-time exchange rates. Just enter the amount and select your currencies to get accurate conversions",
+  },
+  {
+    title: "Todo-Application",
+    desription:
+      "A simple task management app to help your organize to your daily to-do lists. Users can Authentication and authorization Add, edit, delete, and mark tasks as complete and imcomplete with ease.",
+  },
+  {
+    title: "Weather-Application",
+    desription:
+      "Get accurate and real-time weather updates for your location, including a 3-day forecast, temperature, humidity, and more.",
+  },
+];
